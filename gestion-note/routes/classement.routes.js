c8841f99f52af8961a676e0b39d90dd796af1498
Trofel <113066307+lefortnuno@users.noreply.github.com>
2022-11-15 08:43:45 +0300

    Delete gestion-note directory

--- gestion-note/routes/classement.routes.js
+++
@@ -1,5 +0,0 @@
-const router = require("express").Router();
-const noteController = require("../controllers/note.controller");
-router.get("/:niveau", noteController.classementEtudiants);
-
-module.exports = router;
